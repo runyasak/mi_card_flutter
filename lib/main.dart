26839527b20bb462612e5bb993d0517bfb19611c
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-04-10 19:32:11 +0700

    feat; implement StateLessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.white,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
