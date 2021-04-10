06160e925244b3ecfac87a992c575575e9e01f6b
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-04-11 00:15:04 +0700

    feat: implement box-sizing

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
             color: Colors.white,
             child: Text('Hello'),
           ),
